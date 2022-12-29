e3ce47f47f977bfb05ac2cf5603fbfc4bfc94d0b
manish5133 <121598552+manish5133@users.noreply.github.com>
2022-12-29 11:42:31 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,16 @@
+# Security Group
+resource "aws_security_group" "ssh-allowed" {
+    vpc_id = "${aws_vpc.prod-vpc.id}"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+      from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
