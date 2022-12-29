c40013582c8a270f221d1a00d9b47dc2f313592c
manish5133 <121598552+manish5133@users.noreply.github.com>
2022-12-29 11:40:12 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+# variable.tf File
+
+variable "AWS_REGION" {
+   default = "us-east-1"
+}
+
+variable "AMI" {
+   type = map(string)
+   default = {
+      us-east-1 = "ami-002070d43b0a4f171" # Centos 7 HVM
+    }
+}
+
+variable "PUBLIC_KEY_PATH" {
+    default = "~/.ssh/id_rsa.pub" # Ansible Server SSH Key
+}
+
+variable "INSTANCE_TYPE" {
+     default = "t2.micro"
+}
